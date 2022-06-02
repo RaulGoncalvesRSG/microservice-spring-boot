45a71594a45bb0549b40ecb076eea59cdaea60fb
Raul Gonçalves <raulsantos121@gmail.com>
2022-06-02 15:53:45 -0300

    Configuração do zuul api gateway e porta aleatória para hr-payroll

--- hr-api-gateway-zuul/src/main/java/com/raul/demo/HrApiGatewayZuulApplication.java
+++ hr-api-gateway-zuul/src/main/java/com/raul/demo/HrApiGatewayZuulApplication.java
@@ -2,12 +2,16 @@ package com.raul.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableEurekaClient
+@EnableZuulProxy
 @SpringBootApplication
+//Aplicação para fazer o roteamento de microsserviços
 public class HrApiGatewayZuulApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrApiGatewayZuulApplication.class, args);
 	}
-
 }
