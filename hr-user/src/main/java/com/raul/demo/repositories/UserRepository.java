1402b33672315c4c7f06503c48e469fd87ca701e
Raul Gonçalves <raulsantos121@gmail.com>
2022-06-04 11:54:16 -0300

    UserRepository, UserResource e Zuul config

---
+++ hr-user/src/main/java/com/raul/demo/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.raul.demo.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.raul.demo.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	User findByEmail(String email);
+}
